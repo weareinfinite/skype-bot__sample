0cad776cbf0bad7366aae0dc1321a17c5add0b21
Sarath <saratonite@gmx.com>
2017-12-08 09:39:47 +0530

    :ghost: Bot initialised console mode :mens:

---
+++ app.js
@@ -0,0 +1,8 @@
+var builder = require('botbuilder');
+
+
+var connector = new builder.ConsoleConnector().listen();
+var bot = new builder.UniversalBot(connector, function (session) {
+
+    session.send("You said: %s", session.message.text);
+});
