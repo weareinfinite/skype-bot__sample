5248359e3ba7150a21fda6a41170a0dc6a3a293a
Sarath <saratonite@gmx.com>
2017-12-08 09:59:32 +0530

    :dart: Bot Rest Web Service

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
+var restify = require('restify');
 var builder = require('botbuilder');
+require('dotenv').config()
+// Create server
+
+const server = restify.createServer()
 
 
-var connector = new builder.ConsoleConnector().listen();
+server.listen(3000, () => {
+    console.log('%s listening to %s', server.name, server.url)
+})
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+server.post('/api/messages', connector.listen());   
+
+
 var bot = new builder.UniversalBot(connector, function (session) {
 
     session.send("You said: %s", session.message.text);
